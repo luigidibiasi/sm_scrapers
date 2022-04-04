c7f059118fc39795def73f9ced2bac69ba577ad0
Luigi Di Biasi <luigidibiasi@gmail.com>
2022-04-04 21:04:43 +0200

    version 1.0

--- aps.unmc.edu/scraper.py
+++ aps.unmc.edu/scraper.py
@@ -3,6 +3,9 @@ from urllib import request, parse
 import ssl
 import certifi
 import re
+import os
+from bs4 import BeautifulSoup
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 MUST_UPDATE=False;
@@ -25,26 +28,65 @@ if (MUST_UPDATE):
 
 # STEP1: Parse HTML output from link /anti
 # The structure of the HTML seems standard.
-with open("./peptides_name.html") as reader:
-    content = reader.read()
-    # try to extract all <form> tags
-    result = [i for j in re.findall(r'method="([^ >"]*)"|action="([^ >"]*)"|name="([^ >"]*)"',content) for i in j if i]
-    # extract all the peptide ids
-    for t in range(0,len(result),4): 
-        peptide_id = result[t]
-        peptide_id = peptide_id.replace("AP","")
-        # download data regarding each peptide.
-        print("Downloading data for peptide " + str(peptide_id) + " ...")
-        param = {}
-        param['ID']=peptide_id;
-        data = parse.urlencode(param).encode()
-        req =  request.Request("https://aps.unmc.edu/database/peptide", data=data) 
-        resp = request.urlopen(req) #re-enable this to update peptide list in the future
-        with open("./peptides_data/"+peptide_id+".html","w") as writer:
-            html_response = resp.read()
-            writer.write(str(html_response))
-            writer.close()
-            print(html_response)
-        print("\t[DONE]")
+if (MUST_UPDATE):
+    with open("./peptides_name.html") as reader:
+        content = reader.read()
+        # try to extract all <form> tags
+        result = [i for j in re.findall(r'method="([^ >"]*)"|action="([^ >"]*)"|name="([^ >"]*)"',content) for i in j if i]
+        # extract all the peptide ids
+        for t in range(0,len(result),4): 
+            peptide_id = result[t]
+            peptide_id = peptide_id.replace("AP","")
+            # download data regarding each peptide.
+            print("Downloading data for peptide " + str(peptide_id) + " ...")
+            param = {}
+            param['ID']=peptide_id;
+            data = parse.urlencode(param).encode()
+            req =  request.Request("https://aps.unmc.edu/database/peptide", data=data) 
+            resp = request.urlopen(req) #re-enable this to update peptide list in the future
+            with open("./peptides_data/"+peptide_id+".html","w") as writer:
+                html_response = resp.read()
+                writer.write(str(html_response))
+                writer.close()
+                print(html_response)
+            print("\t[DONE]")
 
 #STEP2: For each peptide into download folder ==> parse HTML and extract structured data 
+
+
+OUTPUT = {}
+HEADERS = []
+
+directory = os.fsencode("./peptides_data/")
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     print("Reading: " +filename)
+
+     if not (filename in OUTPUT):
+         OUTPUT[filename] = {}
+
+     with open("./peptides_data/"+filename) as reader:
+         html = reader.read()
+         soup=BeautifulSoup(html,'html.parser')
+         our_tr=soup.find('table').find_all('tr')
+         for TR in our_tr:             
+             TDS=TR.find_all('td')
+             OUTPUT[filename][TDS[0].text]=TDS[1].text;
+             if not (TDS[0].text in HEADERS):
+                 HEADERS.append(TDS[0].text)
+             #print(TDS[0].text)
+             #print(TDS[1].text)
+
+with open("./output.tsv","w") as writer:
+    writer.write("PEPTIDE")
+    for D in HEADERS:
+        D = D.replace('\n',"")
+        writer.write("\t"+D)
+    writer.write("\n")
+    for filename in OUTPUT:
+        writer.write(filename)
+        for D in OUTPUT[filename]:
+            writer.write("\t"+OUTPUT[filename][D])
+        writer.write("\n")
+    writer.close()
+
